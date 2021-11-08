7eb2412cc90204bb20020fa8d69cdac6c6b5c97e
Egor <egor19942012@gmail.com>
2021-11-08 23:01:19 +0530

    add "friend" fixtures

---
+++ src/features/friends-feed/fixtures.ts
@@ -0,0 +1,46 @@
+import {Friend} from './friend';
+
+export const FRIENDS: Friend[] = [
+  {
+    id: 0,
+    firstName: 'Monica',
+    lastName: 'Geller',
+    imageURL: 'https://picsum.photos/200',
+    description: '',
+  },
+  {
+    id: 1,
+    firstName: 'Rachel',
+    lastName: 'Green',
+    imageURL: 'https://picsum.photos/200',
+    description: '',
+  },
+  {
+    id: 2,
+    firstName: 'Ross',
+    lastName: 'Geller',
+    imageURL: 'https://picsum.photos/200',
+    description: '',
+  },
+  {
+    id: 3,
+    firstName: 'Joey',
+    lastName: 'Tribbiani',
+    imageURL: 'https://picsum.photos/200',
+    description: '',
+  },
+  {
+    id: 4,
+    firstName: 'Chandler',
+    lastName: 'Bing',
+    imageURL: 'https://picsum.photos/200',
+    description: '',
+  },
+  {
+    id: 5,
+    firstName: 'Phoebe',
+    lastName: 'Buffay',
+    imageURL: 'https://picsum.photos/200',
+    description: '',
+  },
+];
