946f86911824e8d27cb1abd9ec5c4aea687606c5
Egor <egor19942012@gmail.com>
2021-11-09 22:34:32 +0530

    add friend's remote api

---
+++ src/features/friend-info/friend-api/friend-remote-api.ts
@@ -0,0 +1,19 @@
+import {FriendAPI} from './friend-api';
+import {FriendError} from '../use-case/friend-error';
+import {FriendResult} from '../use-case/get-friend-info';
+import {FRIENDS_MAP} from '../fixtures';
+
+export class FriendsRemoteAPI extends FriendAPI {
+  // Immitates network request. Sometimes throws not found error.
+  getFriend(id: number): Promise<FriendResult> {
+    return new Promise(res => {
+      setTimeout(() => {
+        const randomNumber = Math.random();
+        if (randomNumber < 0.2) {
+          res(new FriendError('No friend found!'));
+        }
+        res(FRIENDS_MAP[id]);
+      }, 2000);
+    });
+  }
+}
--- src/features/friend-info/use-case/get-friend-info.ts
+++ src/features/friend-info/use-case/get-friend-info.ts
@@ -2,7 +2,7 @@ import {Friend} from '../domain/friend';
 import {FriendAPI} from '../friend-api/friend-api';
 import {FriendError} from './friend-error';
 
-type FriendResult = Friend | FriendError;
+export type FriendResult = Friend | FriendError;
 
 export function getFriendInfo(
   api: FriendAPI,
