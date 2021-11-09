06e876e407f616549125570d0476efba9c8a713f
Egor <egor19942012@gmail.com>
2021-11-09 21:01:55 +0530

    update App.js to have a friends controller

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+  StyleSheet,
+} from 'react-native';
+
+import {FriendFeedContainer} from './src/features/friends-feed/friend-feed-container';
+import {FriendsRemoteAPI} from './src/features/friends-feed/friends-remote-api';
+
+const friendsRemoteAPI = new FriendsRemoteAPI();
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <FriendFeedContainer api={friendsRemoteAPI} openFriend={() => {}} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
